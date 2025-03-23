0d8b3afd4255fc1ccf2714ac35da768a871f80f3
Bibhabdas <bibhabdas20@gmail.com>
2025-03-23 11:48:32 +0530

    hypertuning done

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -15,12 +16,21 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     try:
         report={}
         for i in range(len(list(models))):
             model=list(models.values())[i]
+
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
+
+            ## model.fit(X_train,y_train)
             y_train_pred=model.predict(X_train)
             y_test_pred=model.predict(X_test)
 
@@ -28,6 +38,6 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
             test_model_score=r2_score(y_test,y_test_pred)
             report[list(models.keys())[i]]=test_model_score
         return report
-    except:
-        pass
+    except Exception as e:
+        raise CustomException(e, sys)
     
